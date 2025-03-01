c10d03627f2b85e819f5d2baeaa3185c1b56c3c2
Vojavy <sir.strel@gmail.com>
2025-03-01 13:00:10 +0100

    fixed sql

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,22 @@
-CREATE TABLE IF NOT EXISTS hosting_accounts (
+-- Таблица пользователей
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    domain VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,  -- Храним хеш пароля
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+-- Таблица хостинг-аккаунтов
+CREATE TABLE hosting_accounts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,  -- ID владельца аккаунта
+    domain VARCHAR(255) NOT NULL UNIQUE,
+    
     -- Доступ к базе данных
-    username VARCHAR(100) NOT NULL,
-    pass TEXT NOT NULL,
+    user_db VARCHAR(100) NOT NULL,
+    pass_db TEXT NOT NULL,
+    name_db VARCHAR(100) NOT NULL,
     host VARCHAR(255) NOT NULL DEFAULT 'localhost',
 
     -- Доступ к FTP
@@ -12,5 +24,8 @@ CREATE TABLE IF NOT EXISTS hosting_accounts (
     ftp_pass TEXT NOT NULL,
     ftp_home VARCHAR(255) NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    -- Связь с таблицей users
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
